6980a667f11915e4fc647f63610c3696a5e50498
guilheresdl <82681152+guilheresdl@users.noreply.github.com>
2024-10-13 16:30:12 -0300

    Update criarperitagem.js

--- criarperitagem.js
+++ criarperitagem.js
@@ -1,91 +1,53 @@
-// Script para adicionar funcionalidades à página "Criar Peritagem"
-document.addEventListener('DOMContentLoaded', () => {
-    const pecaSelect = document.getElementById('pecas');
-    const opcoesPecaDiv = document.getElementById('opcoesPeca');
+document.getElementById('pecas').addEventListener('change', function () {
+    const pecaSelecionada = this.value;
     const acoesDisponiveisDiv = document.getElementById('acoesDisponiveis');
-    const adicionarPecaBtn = document.getElementById('adicionarPeca');
-    const tabelaCorpo = document.getElementById('tabelaCorpo');
 
-    pecaSelect.addEventListener('change', () => {
-        const peca = pecaSelect.value;
-        opcoesPecaDiv.classList.remove('hidden');
-        acoesDisponiveisDiv.innerHTML = ''; // Limpa opções anteriores
+    // Limpar ações anteriores
+    acoesDisponiveisDiv.innerHTML = '';
 
-        // Definindo as ações e inputs baseados na peça selecionada
-        let acoesHtml = '';
-        if (peca === 'haste') {
-            acoesHtml += `
-                <label><input type="checkbox" value="fabricar"> Fabricar</label>
-                <label><input type="checkbox" value="cromar"> Cromar</label>
-                <label><input type="checkbox" value="recRosca"> Recuperar Rosca</label>`;
-        } else if (peca === 'camisa') {
-            acoesHtml += `
-                <label><input type="checkbox" value="fabricar"> Fabricar</label>
-                <label><input type="checkbox" value="brunir"> Brunir</label>
-                <label><input type="checkbox" value="recRosca"> Recuperar Rosca</label>`;
-        } else if (peca === 'olhal') {
-            acoesHtml += `
-                <label><input type="checkbox" value="fabricar"> Fabricar</label>
-                <label><input type="checkbox" value="recFuro"> Recuperar Furo</label>
-                <label><input type="checkbox" value="recRosca"> Recuperar Rosca</label>`;
-        } else if (peca === 'flange') {
-            acoesHtml += `
-                <label><input type="checkbox" value="fabricar"> Fabricar</label>
-                <label><input type="checkbox" value="recuperar"> Recuperar</label>`;
-        } else if (peca === 'fundo') {
-            acoesHtml += `
-                <label><input type="checkbox" value="fabricar"> Fabricar</label>
-                <label><input type="checkbox" value="recuperar"> Recuperar</label>`;
-        } else if (peca === 'embolo') {
-            acoesHtml += `
-                <label><input type="checkbox" value="fabricar"> Fabricar</label>
-                <label><input type="checkbox" value="recuperar"> Recuperar</label>`;
-        } else if (peca === 'espacador') {
-            acoesHtml += `
-                <label><input type="checkbox" value="fabricar"> Fabricar</label>
-                <label><input type="checkbox" value="recuperar"> Recuperar</label>`;
-        } else if (peca === 'jogoVedacao') {
-            acoesHtml += `
-                <label><input type="checkbox" value="fabricar"> Fabricar</label>`;
-        }
+    let acoes = [];
 
-        acoesDisponiveisDiv.innerHTML = acoesHtml;
-    });
-
-    adicionarPecaBtn.addEventListener('click', () => {
-        const peca = pecaSelect.value;
-        const diametro = document.getElementById('diametro').value;
-        const comprimento = document.getElementById('comprimento').value;
-        const largura = document.getElementById('largura').value;
-
-        if (peca && diametro && comprimento && largura) {
-            const novaLinha = `
-                <tr>
-                    <td>${peca}</td>
-                    <td>${getAcoesSelecionadas()}</td>
-                    <td>${diametro}</td>
-                    <td>${comprimento}</td>
-                    <td>${largura}</td>
-                </tr>`;
-            tabelaCorpo.insertAdjacentHTML('beforeend', novaLinha);
-            // Limpa os campos após adicionar
-            clearFields();
-        } else {
-            alert("Por favor, preencha todos os campos antes de adicionar.");
-        }
-    });
-
-    function getAcoesSelecionadas() {
-        const acoesCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-        return Array.from(acoesCheckboxes).map(checkbox => checkbox.value).join(', ');
+    // Definindo ações de acordo com a peça selecionada
+    switch (pecaSelecionada) {
+        case 'haste':
+            acoes = ['Fabricar', 'Cromar', 'Rec. Rosca'];
+            break;
+        case 'camisa':
+            acoes = ['Fabricar', 'Brunir', 'Rec. Rosca'];
+            break;
+        case 'olhal':
+            acoes = ['Fabricar', 'Rec. Furo', 'Rec. Rosca'];
+            break;
+        case 'flange':
+            acoes = ['Fabricar', 'Recuperar'];
+            break;
+        case 'fundo':
+            acoes = ['Fabricar', 'Rec. Olhal'];
+            break;
+        case 'embolo':
+            acoes = ['Fabricar', 'Rec. Olhal', 'Rec. Rosca'];
+            break;
+        case 'espacador':
+            acoes = ['Fabricar', 'Recuperar'];
+            break;
+        case 'jogoDeVedacao':
+            acoes = ['Substituir', 'Cliente Vai Fornecer'];
+            break;
     }
 
-    function clearFields() {
-        pecaSelect.value = '';
-        document.getElementById('diametro').value = '';
-        document.getElementById('comprimento').value = '';
-        document.getElementById('largura').value = '';
-        opcoesPecaDiv.classList.add('hidden');
-        acoesDisponiveisDiv.innerHTML = '';
-    }
+    // Gerando checkboxes para as ações
+    acoes.forEach(acao => {
+        const divAcao = document.createElement('div');
+        divAcao.className = 'acao-disponivel';
+        
+        divAcao.innerHTML = `
+            <input type="checkbox" id="${acao}" name="acao" value="${acao}">
+            <label for="${acao}">${acao}</label>
+        `;
+        
+        acoesDisponiveisDiv.appendChild(divAcao);
+    });
+
+    // Exibir a seção de ações disponíveis
+    document.getElementById('opcoesPeca').classList.remove('hidden');
 });
